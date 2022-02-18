09e7f7a32f2bdb945272c5e8b9e01fbacae9bb73
黄冰 <2902978956@qq.com>
2022-02-18 17:49:21 +0800

    用户注册校验验证码逻辑

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,15 +1,36 @@
 import {Context} from 'koa';
 import {getManager} from 'typeorm';
 import {User} from '../entity/User';
+import {Code} from '../entity/Code';
 
-export const createUser = async (context: Context) => {
-  const userRepository = getManager().getRepository(User);
-  const newUser = userRepository.create({
-    username: `burt${Math.random() * 10}`.slice(0, 9),
-    password: '123456',
-    email: `${Math.random() * 10}@163.com`
-  });
-  await userRepository.save(newUser);
+class UserController {
+  // 创建用户
+  async createUser(context: Context) {
+    const {username, email, code} = context.request.body;
+    const result = await UserController.#validateCode(code, email);
+    if (!result) context.throw(400, '验证码有误');
 
-  context.body = newUser;
-};
+    const userRepository = getManager().getRepository(User);
+    const newUser = userRepository.create({
+      username: `burt${Math.random() * 10}`.slice(0, 9),
+      password: '123456',
+      email: `${Math.random() * 10}@163.com`
+    });
+    await userRepository.save(newUser);
+
+    context.body = newUser;
+  }
+
+  // 校验验证码
+  static async #validateCode(authCode: string, email: string) {
+    const codeRepository = getManager().getRepository(Code);
+    const codeObject = await codeRepository.findOne({where: {email}});
+    if (!codeObject || codeObject.code !== authCode) {
+      return false;
+    }
+    return Date.now() - new Date(codeObject.updatedAt).getDate() <= 5 * 60 * 1000;
+
+  }
+}
+
+export default new UserController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
 import {welcome} from './controller/welcome';
 import CodeController from './controller/code';
 
+const prefixMap = {
+  code: '/code',
+  user: '/user'
+};
+
 export const AppRoutes = [
   {path: '/', method: 'get', action: welcome},
   // 发送验证码
-  {path: '/code/send', method: 'post', action: CodeController.send},
+  {path: `${prefixMap.code}/send`, method: 'post', action: CodeController.send},
 ];
